cf16970e0bf4a08cc3b81af548bdf783b37d29f5
John <johnfrivera1309@gmail.com>
2024-08-08 11:32:55 -0500

    Remodelado de pagina Servicios en Dashboard

--- pages/admin/servicios/index.php
+++ pages/admin/servicios/index.php
@@ -16,78 +16,109 @@ $users = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.0.8/b-3.0.2/b-html5-3.0.2/b-print-3.0.2/datatables.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="shortcut icon" href="../../assets/img/spa_logo.webp" type="image/x-icon">
+    <link rel="stylesheet" href="../../../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../assets/css/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../../assets/css/datatables.min.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
     <title>Spa | Gestión de <?php echo PAGE_NAME ?></title>
 </head>
 
-<body class="container-fluid bg-body-secondary">
+<body>
     <?php include("../assets/templates/header.php"); ?>
-
-    <main class="row">
-        <?php include("../assets/templates/aside_bar.php"); ?>
-
-        <section class="col p-4">
-            <h5 class="fw-normal mb-0">Bienvenido a</h5>
-            <h4 class="fw-bold mb-4">Gestión de <?php echo PAGE_NAME ?></h4>
-            <div class="row">
-                <div class="col">
-                    <div class="bg-light rounded-4 shadow-sm p-3">
-                        <?php include("../assets/templates/nav_pills.php") ?>
-
-                        <div class="table-responsive mb-2">
-                            <table id="myTable" class="table table-light table-hover w-100 mb-0">
-                                <thead>
-                                    <tr>
-                                        <th>Descripción</th>
-                                        <th>Terapeuta</th>
-                                        <th>
-                                            <i class="bi bi-alarm me-2"></i>
-                                            Duración
-                                        </th>
-                                        <th>
-                                            <i class="bi bi-currency-dollar me-2"></i>
-                                            Precio
-                                        </th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    foreach ($users as $key => $user) {
-                                    ?>
+    <main class="container-fluid">
+        <div class="row bg-body-secondary h-section">
+            <?php include("../assets/templates/aside_bar.php"); ?>
+            <section class="col p-4">
+                <h5 class="fw-normal mb-0">Bienvenido a</h5>
+                <h4 class="fw-bold mb-4">Gestión de <?php echo PAGE_NAME ?></h4>
+                <div class="row">
+                    <div class="col">
+                        <div class="bg-light rounded-4 shadow-sm p-3">
+                            <?php include("../assets/templates/nav_pills.php") ?>
+                            <div class="table-responsive mb-2">
+                                <table id="myTable" class="table table-light table-hover w-100 fs-5 mb-0">
+                                    <thead>
                                         <tr>
-                                            <td class="opacity-75"><?php echo $user["descripcion"] ?></td>
-                                            <td class="opacity-75"><?php echo $user["terapeuta"] ?></td>
-                                            <td class="opacity-75"><?php echo $user["duracion"] ?></td>
-                                            <td class="opacity-75"><?php echo $user["precio"] ?></td>
-                                            <td class="opacity-75">
-                                                <div class="d-flex justify-content-end gap-2">
-                                                    <a href=<?php echo "./editar.php?nombres=" . $user["nombre"] . "&apellidos=" . $user["apellido"] . "&telefono=" . $user["telefono"] . "&correo=" . $user["correo"] ?> class="link-primary"><i class="bi bi-pencil-square"></i></a>
-                                                    <a href=<?php echo "./eliminar.php?id=" . $user["id"] . "&nombre=" . $user["nombre"] . "+" . $user["apellido"] ?> class="link-danger"><i class="bi bi-trash-fill"></i></a>
-                                                </div>
-                                            </td>
+                                            <th>Descripción</th>
+                                            <th>Terapeuta</th>
+                                            <th>
+                                                <i class="bi bi-alarm me-2"></i>
+                                                Duración
+                                            </th>
+                                            <th>
+                                                <i class="bi bi-currency-dollar me-2"></i>
+                                                Precio
+                                            </th>
+                                            <th></th>
                                         </tr>
-                                    <?php
-                                    }
-                                    ?>
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        foreach ($users as $key => $user) {
+                                        ?>
+                                            <tr id="row-<?php echo $user["id"] ?>">
+                                                <td><?php echo $user["descripcion"] ?></td>
+                                                <td><?php echo $user["terapeuta"] ?></td>
+                                                <td><?php echo $user["duracion"] ?></td>
+                                                <td><?php echo $user["precio"] ?></td>
+                                                <td>
+                                                    <div class="d-flex justify-content-end gap-2">
+                                                        <button type="button" onclick="showPutModal(<?php echo $user['id'] ?>)" class="bg-transparent border-0 text-primary p-0">
+                                                            <i class="bi bi-pencil-square"></i>
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        <?php
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                                <div class="modal fade" id="putModal" tabindex="-1" aria-labelledby="putModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <form id="frmPut" action="/spa/back/" method="post" class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title text-primary fs-4" id="putModalLabel">Modificar Servicio</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <input type="text" name="descripcion" id="inpDescripcion" class="form-control form-control-lg mb-2" placeholder="Descripción" required>
+                                                <div class="input-group rounded-3 mb-2">
+                                                    <div class="input-group-text">
+                                                        <i class="bi bi-alarm fs-5"></i>
+                                                    </div>
+                                                    <input type="text" name="duracion" id="inpDuracion" class="form-control form-control-lg" placeholder="Duración" pattern="^[0-9]*$" min="1" required>
+                                                </div>
+                                                <div class="input-group rounded-3 mb-2">
+                                                    <div class="input-group-text">
+                                                        <i class="bi bi-currency-dollar fs-5"></i>
+                                                    </div>
+                                                    <input type="text" name="precio" id="inpPrecio" class="form-control form-control-lg" placeholder="Precio" pattern="^[0-9]*$" min="0" required>
+                                                </div>
+                                                <select name="terapeuta" id="slcTerapeuta" class="form-select form-select-lg">
+                                                    <option>Terapeuta...</option>
+                                                </select>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                                <button type="submit" class="btn btn-primary">Modificar</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </main>
-
-    <?php include("../../assets/templates/footer.php"); ?>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.0.8/b-3.0.2/b-html5-3.0.2/b-print-3.0.2/datatables.min.js"></script>
+    <?php include("../../../assets/templates/footer.php"); ?>
+    <script src="../../../assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../../assets/js/vfs_fonts.js"></script>
+    <script src="../../assets/js/pdfmake.min.js"></script>
+    <script src="../../assets/js/datatables.min.js"></script>
     <script>
         let table = new DataTable('#myTable', {
             language: {
@@ -105,6 +136,32 @@ $users = [
                 }
             }
         });
+        const putModal = new bootstrap.Modal('#putModal');
+        const getCols = (id) => {
+            var row = document.getElementById(`row-${id}`).children;
+            var cols = [];
+            for (let i = 0; i < (row.length - 1); i++) {
+                cols.push(row.item(i).innerText);
+            };
+            return cols;
+        };
+        const selectOpt = (elementId, value) => {
+            var select = document.getElementById(elementId);
+            for (let i = 0; i < select.length; i++) {
+                if (select.item(i).value == value) {
+                    select.item(i).selected = true;
+                };
+            };
+        };
+        const showPutModal = (id) => {
+            var cols = getCols(id);
+            document.getElementById("frmPut").action += id;
+            document.getElementById("inpDescripcion").value = cols[0];
+            document.getElementById("inpDuracion").value = cols[2];
+            document.getElementById("inpPrecio").value = cols[3];
+            selectOpt("slcTerapeuta", cols[1]);
+            putModal.show();
+        };
     </script>
 </body>
 
