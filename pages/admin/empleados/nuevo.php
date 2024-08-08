c13810c530a9a8e6d865e550a8b6d063bc35b28f
John <johnfrivera1309@gmail.com>
2024-08-08 11:06:37 -0500

    Remodelado de pagina Empleados en Dashboard

--- pages/admin/empleados/nuevo.php
+++ pages/admin/empleados/nuevo.php
@@ -8,51 +8,50 @@ define("PILL_SELECT", "Nuevo");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="shortcut icon" href="../../assets/img/spa_logo.webp" type="image/x-icon">
+    <link rel="stylesheet" href="../../../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../assets/css/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../../assets/css/datatables.min.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
     <title>Spa | Gestión de <?php echo PAGE_NAME ?></title>
 </head>
 
-<body class="container-fluid bg-body-secondary">
+<body>
     <?php include("../assets/templates/header.php"); ?>
-
-    <main class="row">
-        <?php include("../assets/templates/aside_bar.php"); ?>
-
-        <section class="col p-4">
-            <h5 class="fw-normal mb-0">Bienvenido a</h5>
-            <h4 class="fw-bold mb-4">Gestión de <?php echo PAGE_NAME ?></h4>
-            <div class="row">
-                <div class="col">
-                    <div class="bg-light rounded-4 shadow-sm p-3">
-                        <?php include("../assets/templates/nav_pills.php") ?>
-
-                        <div class="row mt-3">
-                            <form action="" method="post" class="col-12 col-md-9 col-lg-5">
-                                <div class="row g-2 mb-2">
-                                    <div class="col">
-                                        <input type="text" name="nombres" class="form-control rounded-3" placeholder="Nombres" required>
+    <main class="container-fluid">
+        <div class="row bg-body-secondary h-section">
+            <?php include("../assets/templates/aside_bar.php"); ?>
+            <section class="col p-4">
+                <h5 class="fw-normal mb-0">Bienvenido a</h5>
+                <h4 class="fw-bold mb-4">Gestión de <?php echo PAGE_NAME ?></h4>
+                <div class="row">
+                    <div class="col">
+                        <div class="bg-light rounded-4 shadow-sm p-3">
+                            <?php include("../assets/templates/nav_pills.php") ?>
+                            <div class="row mt-3">
+                                <form action="" method="post" class="col-12 col-md-9 col-lg-5">
+                                    <div class="row g-2 mb-2">
+                                        <div class="col">
+                                            <input type="text" name="nombres" class="form-control form-control-lg" placeholder="Nombres" required>
+                                        </div>
+                                        <div class="col">
+                                            <input type="text" name="apellidos" class="form-control form-control-lg" placeholder="Apellidos" required>
+                                        </div>
                                     </div>
-                                    <div class="col">
-                                        <input type="text" name="apellidos" class="form-control rounded-3" placeholder="Apellidos" required>
-                                    </div>
-                                </div>
-                                <input type="text" name="especialidad" class="form-control rounded-3" placeholder="Especialidad" required>
-                                <p id="txtErr" class="text-danger fw-semibold ms-1 mb-0"></p>
-                                <button class="btn btn-primary w-100 mt-3" type="submit">
-                                    Crear
-                                </button>
-                            </form>
+                                    <input type="text" name="especialidad" class="form-control form-control-lg mb-3" placeholder="Especialidad" required>
+                                    <button type="submit" class="btn btn-lg btn-success w-100">
+                                        Crear
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </main>
-
-    <?php include("../../assets/templates/footer.php"); ?>
+    <?php include("../../../assets/templates/footer.php"); ?>
+    <script src="../../../assets/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
